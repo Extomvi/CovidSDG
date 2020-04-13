b8d58bdd08f2e591966e46ec7213b349a24f3643
Extomvi <exceltadedokun@gmail.com>
2020-04-13 15:33:22 +0100

    'app update'

--- src/app.py
+++ src/app.py
@@ -17,26 +17,6 @@ def add_headers(response):
 def hello(name):
     return "Hello! This is a {}.".format(name) 
 
-@app.route('/response', methods=['GET'])
-def api_response():
-   
-   data = {
-       region: {
-           name: "Africa",
-           avgAge: 19.7,
-           avgDailyIncomeInUSD: 5,
-           avgDailyIncomePopulation: 0.71
-        },
-       periodType: "days",
-       timeToElapse: 58,
-       reportedCases: 674,
-       population: 66622705,
-       totalHospitalBeds: 1380614
-    }
-   js = json.dumps(data)
-   resp = severeImpact(js)
-   return resp
-
 if __name__ == "__main__":
 #    impact()
     severeImpact()
