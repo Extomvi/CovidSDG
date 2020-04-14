af7888253c371b1229f206fcbf73e16978b4b332
Extomvi <exceltadedokun@gmail.com>
2020-04-14 19:08:11 +0100

    'Update'

---
+++ src/test.py
@@ -0,0 +1,16 @@
+import unittest
+import math
+from unittest import TestCase
+from estimator import severeImpact
+
+
+class EstimatorTest(unittest.TestCase):
+            
+    def test_power(self):
+        self.assertEquals(pow(5, 2), 25)
+
+
+
+ 
+if __name__ == '__main__':
+    unittest.main()
